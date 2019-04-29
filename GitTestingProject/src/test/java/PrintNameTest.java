510557ab6e91a56ada968ec6bf3d5fb532d8ea45
sonamSh <sonamsharma.123789@gmail.com>
2019-04-29 21:35:24 +0530

    shsh

--- GitTestingProject/src/test/java/PrintNameTest.java
+++ GitTestingProject/src/test/java/PrintNameTest.java
@@ -2,7 +2,7 @@ public class PrintNameTest {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             System.out.println("sonam");
-            System.out.println("sharma");
+            System.out.println("sharma sharma");
         }
     }
 }
